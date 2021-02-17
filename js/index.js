dcb56d987eeab652df89411cf1332e157fa47c0b
Elisha Avior <64099551+eavior@users.noreply.github.com>
2021-02-17 21:16:52 +0200

    M7 (WIP)

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
-// function fibonacci(num) {
-//   num = document.getElementById("inputNumber").value;
-//   let a = 0, b = 1, c = 0;
-//   for (let i = 0; i < num; i++) {
-//     a = b;
-//     b = c;
-//     c = a + b;
-//   }
-//   document.getElementById("outputNumber").innerText = c;
-// }
+function getFibonacciLocally(num) {
+  let a = 0,
+    b = 1,
+    c = 0;
+  for (let i = 0; i < num; i++) {
+    a = b;
+    b = c;
+    c = a + b;
+  }
+  document.getElementById("result").innerHTML = `<div id="outputNumber">${c}</div>`;
+}
 
-async function getFibonacci() {
-  document.getElementById("inputNumber").classList.remove("is-invalid");
-  document.getElementById("alert").innerHTML = `<div></div>`;
-  document.getElementById("result").innerHTML = `<div></div>`;
-
-  num = document.getElementById("inputNumber").value;
+async function getFibonacciFromServer(num) {
   if (num > 50) {
     document.getElementById("inputNumber").classList.add("is-invalid");
-    document.getElementById("alert").innerHTML = `<div class="col-sm-3"><small class="text-danger">Can't be larger than 50</small></div>`;
+    // document.getElementById("alert").innerHTML = `<small class="text-danger">Can't be larger than 50</small>`;
+    var alertText = document.createElement("small");   // Create a <button> element
+    alertText.innerHTML = "Can't be larger than 50";                   // Insert text
+    document.getElementById("alert").appendChild(alertText).className = "text-danger"; 
+    document.getElementById("checkbox").classList.add("form-check-push-down");  
+    document.getElementById("checkbox").classList.remove("form-check");              // Append <button> to <body>
   } else {
     document.getElementById("result").innerHTML = `<div class="spinner1 spinner-border" role="status"><span class="sr-only">Loading...</span></div>`;
     await fetch(`http://localhost:5050/fibonacci/${num}`).then(response => {
@@ -31,7 +32,6 @@ async function getFibonacci() {
         })
       }
     })
-
   }
   loadFibonacciResults()
 }
@@ -43,7 +43,7 @@ function loadFibonacciResults() {
       response.json().then(data => {
         for (let fResults of data.results) {
           let cDate = new Date(fResults.createdDate);
-          document.getElementById("results").innerHTML += `<li>The Fibonnaci of <mark>${fResults.number}</mark> is <mark>${fResults.result}</mark>. Calculated at: ${cDate.toString()}</li>`;
+          document.getElementById("results").innerHTML += `<li>The Fibonnaci of <strong>${fResults.number}</strong> is <strong>${fResults.result}</strong>. Calculated at: ${cDate.toString()}</li>`;
         }
         document.getElementById("resultsSpinner").innerHTML = ``;
         return;
@@ -51,7 +51,61 @@ function loadFibonacciResults() {
     });
 }
 
+function cleanOutputs() {
+  document.getElementById("inputNumber").classList.remove("is-invalid");
+  document.getElementById("alert").innerHTML = "";
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("checkbox").classList.add("form-check");
+  document.getElementById("checkbox").classList.remove("form-check-push-down");
+}
+
 loadFibonacciResults();
+
+let server = false;
+let checkbox = document.getElementById('flexCheckDefault');
+checkbox.addEventListener('change', e => {
+  if (e.target.checked) server = true;
+  else server = false;
+});
 
 const myButton = document.getElementById('fButton');
 myButton.addEventListener('click', getFibonacci);
+
+function getFibonacci() {
+  cleanOutputs();
+  let num = document.getElementById("inputNumber").value;
+  if (server) getFibonacciFromServer(num);
+  else getFibonacciLocally(num);
+}
+
+// const myCheckbox = document.getElementById('flexCheckDefault');
+// myCheckbox.addEventListener('checked', alert("hello"));
+
+
+
+
+
+
+
+// FEEDBACK RAZ:
+
+// const getResultsUrl = ('http://localhost:5050/getFibonacciResults')
+
+// async function getResultsUrl();
+//   const response = await fetch(getResultsUrl);
+//   console.log(response.json());
+//   const data = await response.json();
+//   console.log(data.results);
+// }
+// getResults();
+
+// async function getResultsUrl();
+//   const response = await fetch(getResultsUrl);
+//   let data;
+//   //if (response.ok) {
+//   data = await response.json();
+//   console.log(data.results);
+//   } else {
+//     data = response.text();
+//   }
+// }
